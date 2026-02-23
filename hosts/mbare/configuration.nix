9bfd73a438f6b9c7ba497041fcb27ef18f9aad34
Your Name <you@example.com>
2026-02-23 16:41:55 +0000

    Added experimental features to mbare

--- hosts/mbare/configuration.nix
+++ hosts/mbare/configuration.nix
@@ -15,6 +15,8 @@
 
   nixpkgs.config.allowUnfree = true;
 
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   imports = [
     ../../modules/common/immutable-users.nix
     ../../modules/desktop/hyprland.nix
