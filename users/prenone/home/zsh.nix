d67067d497415b6f0901a267a80e11ec07116853
Achille Merendino <prenone03@gmail.com>
2026-02-23 18:40:22 +0000

    Added fzf

--- users/prenone/home/zsh.nix
+++ users/prenone/home/zsh.nix
@@ -2,11 +2,16 @@
 
 {
   programs.zsh = {
-  enable = true;
-  history = {
-    size = 10000;
-    save = 10000;
-    path = "$HOME/.zsh_history";
+    enable = true;
+    history = {
+      size = 10000;
+      save = 10000;
+      path = "$HOME/.zsh_history";
+    };
   };
-};
+
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
+  };
 }
