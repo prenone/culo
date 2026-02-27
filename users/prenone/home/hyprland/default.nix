305d83d6908674c8f3a37b3f6eb6bc27edc500d6
Achille Merendino <prenone03@gmail.com>
2026-02-27 20:04:23 +0100

    Added workspace bindings and removed animations

--- users/prenone/home/hyprland/default.nix
+++ users/prenone/home/hyprland/default.nix
@@ -26,6 +26,16 @@
 
       bind = [
         "$mod, Return, exec, wofi"
+        "$mod, 1, workspace, 1"
+        "$mod, 2, workspace, 2"
+        "$mod, 3, workspace, 3"
+        "$mod, 4, workspace, 4"
+        "$mod, 5, workspace, 5"
+        "$mod, 6, workspace, 6"
+        "$mod, 7, workspace, 7"
+        "$mod, 8, workspace, 8"
+        "$mod, 9, workspace, 9"
+        "$mod, 0, workspace, 10"
       ];
 
       bindel = [
@@ -46,6 +56,10 @@
 
       input = {
         touchpad.natural_scroll = true;
+      };
+
+      animations = {
+        enabled = false;
       };
 
       xwayland = {
